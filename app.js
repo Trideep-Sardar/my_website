63b49906a2924bfecc45aba9d884991446fee686
Trideep <trideepsardar@gmail.com>
2020-11-26 15:27:59 +0530

    adding .gitignore and changes in app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const app=express();
 const path=require('path');
-const port=80;
+const port=process.env.PORT ||80;
 
 app.use('/static',express.static('static'));
 app.use(express.urlencoded({extended:true}));
